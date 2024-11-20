7e427058e6b56a81583286786229e2bec64db768
Joe Tynan <tynanj@outlook.com>
2024-11-20 13:38:57 +0000

    Adding Frontend

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {request} = require("express");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'));
 
 morgan.token('body', request => {
     return JSON.stringify(request.body);
